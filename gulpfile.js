e23f1b8f75084ea6fccfbd09480419ed5a6278de
AfRiyy <AfRiyy@gmail.com>
2021-09-23 09:10:21 +0200

    alap

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const { src, dest, watch } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const celanCSS = require('gulp-clean-css');
 
 function streamHtml() {
 	return src('src/*.html')
@@ -6,17 +9,27 @@ function streamHtml() {
 }
 function streamJS() {
 	return src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
 		.pipe(dest('public/js'));
 }
 function streamJquery() {
     return src('node_modules/jquery/dist/jquery.min.js')
+        .pipe(uglify())
         .pipe(dest('public/lib'));
 }
 
+function streamCss(){
+    return src('src/css/*.css')
+        .pipe(celanCSS())
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(dest('public/css'));
+}
 
 exports.jquery = streamJquery;
 exports.html= streamHtml;
 exports.default = function() {
     watch('src/*.html', streamHtml )
     watch('src/js/*.js', streamJS )
+    watch('src/css/*.css', streamCss )
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
+/**
+ * A saját JavaScript fájl
+ */
+
 $('h1').html('Négy');
+
+console.log('alma');
+console.log('körte');
+
+function fa(){
+    bokor();
+}
+function bokor(){
+    console.log('tuja');
+}
+fa();
